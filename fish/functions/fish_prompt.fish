201b46ac6a8ba235aaf8662bf0cc78a3b4196f47
Miliaa12 <c.e.henriksson@gmail.com>
2024-06-07 18:18:43 +0200

    Merges prompt and right prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,5 +3,31 @@ function fish_prompt
 	set -l dir (set_color $fish_color_cwd)"["(prompt_pwd)"]"
 	set -l git (set_color green)(fish_vcs_prompt)
 	echo $USER $dir $time $git
-	echo (set_color white)"❯❯❯ " 
+	echo (set_color white)"❯❯❯ "
+end
+
+function kubectl_status
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
+    return
+  end
+
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
+    return
+  end
+
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
+
+  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
+end
+
+function fish_right_prompt
+  echo (kubectl_status)
 end
